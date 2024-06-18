9e25035f04efd21f9afa133b1885565a9f2ec01b
scott <scott@gmail.com>
2024-06-18 20:18:29 +0530

    first commit

---
+++ src/main/java/in/ravi/awsdeploy/SpringbootAwsdeployApplication.java
@@ -0,0 +1,13 @@
+package in.ravi.awsdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsdeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsdeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ravi/awsdeploy/SpringbootAwsdeployApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ravi.awsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsdeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
